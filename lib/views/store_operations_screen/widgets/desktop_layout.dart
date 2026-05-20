c0ecf768c6edc81f4ca0452b39e9c259475c995c
salmanf.rahman <salmanfarid43@gmail.com>
2026-05-20 14:28:54 +0600

    redesign: desktop layout with refined spacing

--- lib/views/store_operations_screen/widgets/desktop_layout.dart
+++ lib/views/store_operations_screen/widgets/desktop_layout.dart
@@ -16,27 +16,25 @@ class DesktopLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(24, 0, 24, 24),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Credit Grams Panel
           Expanded(
             child: SingleChildScrollView(
               child: creditGramsPanel
                   .animate()
-                  .fadeIn(delay: 100.ms, duration: 600.ms)
-                  .slideX(begin: -0.1, end: 0, duration: 600.ms),
+                  .fadeIn(delay: 100.ms, duration: 500.ms)
+                  .slideX(begin: -0.05, end: 0, duration: 500.ms),
             ),
           ),
-          const SizedBox(width: 16),
-          // Redeem Code Panel
+          const SizedBox(width: 20),
           Expanded(
             child: SingleChildScrollView(
               child: redeemCodePanel
                   .animate()
-                  .fadeIn(delay: 200.ms, duration: 600.ms)
-                  .slideX(begin: 0.1, end: 0, duration: 600.ms),
+                  .fadeIn(delay: 150.ms, duration: 500.ms)
+                  .slideX(begin: 0.05, end: 0, duration: 500.ms),
             ),
           ),
         ],
